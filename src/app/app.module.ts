4145d52cc46e32ab643ed3f3a72b0e84083acda1
Leonardo Pimentel Lopes Filho <leo_zinho_filho@hotmail.com>
2023-04-13 22:56:54 -0300

    Aula13abril

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SchoolComponent } from './school/school.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule
